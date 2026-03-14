243cc276d8ccfdbe5428d3b3f8a65cbb02d367f7
Szymon Nowicki <s.nowick@wp.pl>
2026-03-14 21:20:16 +0100

    More advanced guessing game

--- main.cpp
+++ main.cpp
@@ -3,11 +3,26 @@
 //
 
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 int main() {
     int guess;
     cout << "Guess a number!" << endl;
     cin >> guess;
     cout << "You guessed:" << guess << endl;
+
+    srand(time(0));
+    int secret = rand() % 100+1;
+
+    if (guess > secret) {
+        cout << "Too high!" << endl;
+    }
+    else if (guess < secret) {
+        cout << "Too low!" << endl;
+    }
+    else {
+        cout << "You win!" << endl;
+    }
+    cout << "The number was " << secret << endl;
     return 0;
 }
