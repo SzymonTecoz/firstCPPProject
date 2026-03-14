5c4853e9bc6b4e7777b71d8824609723e4e8abc8
Szymon Nowicki <s.nowick@wp.pl>
2026-03-14 21:15:28 +0100

    Added guessing number

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Szymon Nowicki on 14/03/2026.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+    int guess;
+    cout << "Guess a number!" << endl;
+    cin >> guess;
+    cout << "You guessed:" << guess << endl;
+    return 0;
+}
